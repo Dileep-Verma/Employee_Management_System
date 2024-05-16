32be5016ccc65e9d500beadf3a67f21d1d6b0bce
Dileep-Verma <dv936998@gmail.come>
2024-05-16 16:25:37 +0530

    in file index.php search query logic to implement

--- index.php
+++ index.php
@@ -2,6 +2,28 @@
 
  ?>
 
+<?php
+//search logic
+
+if(isset($_POST["searchdata"])){
+
+    $search=$_POST['search'];
+
+    $sql="SELECT * FROM employee  WHERE id='$search'";
+    $data=mysqli_query($conn,$sql);
+
+   // $row=mysqli_num_rows($data);
+
+       $result= mysqli_fetch_assoc($data);
+      // $name=$result["name"];
+     // echo $result['name'];
+     // echo $name;
+
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,51 +35,94 @@
 <link rel="stylesheet" href="style.css">
 
 <body>
-<form method="POST" action="#">
+
     <div class="center">
-        <h1>Employee Data Entry Automation Software</h1>
-       
+        <form method="POST" action="#">
+            <h1>Employee Data Entry Automation Software</h1>
+
             <div class="form">
 
-                <input type="text" name="id" placeholder="Search ID" class="textfield">
+                <input type="text" name="search" placeholder="Search ID" class="textfield"
+                    value="<?php if(isset($_POST['searchdata'])){ echo $result['id']; }?>">
 
-                <input type="text" name="name" placeholder="Employee Name" class="textfield">
+                <input type="text" name="name" placeholder="Employee Name" class="textfield"
+                    value="<?php if(isset($_POST['searchdata'])){ echo $result['name']; }?>">
 
 
-                <select name="gender" class="textfield">
-                    <option value="Not Select">Select Gender</option>
-                    <option value="Male">Male</option>
-                    <option value="Female">Female</option>
-                    <option value="Other">Other</option>
+                <select class="textfield" name="gender">
+                    <option value="Not Selected">Select Gender</option>
+                    <option value="Male" <?php 
+                    if($result['gender'] == 'Male') 
+                    {
+                        echo "selected";
+                    }
+                    ?>>Male</option>
+
+
+                    <option value="Female" <?php 
+                    
+                    if($result['gender']=='Female') 
+                    {
+                        echo"selected";
+                    }
+                    ?>>Female</option>
+                    <option value="Other" <?php 
+                    if($result['gender'] == 'Other'){
+                        echo "selected";
+                    }
+                    ?>>Other</option>
                 </select>
 
 
-                <input type="text" name="email" placeholder="Email" class="textfield">
+                <input type="text" name="email" placeholder="Email" class="textfield" value="<?php if(isset($_POST['searchdata'])){
+                    echo $result['email'];}?>">
 
 
-                <input type="text" name="salary" placeholder="Employee Salary" class="textfield">
+                <input type="text" name="salary" placeholder="Employee Salary" class="textfield"
+                    value="<?php if(isset($_POST['searchdata'])){ echo $result['salary']; }?>">
 
                 <select name="department" class="textfield">
                     <option value="Not Select">Select Department</option>
-                    <option value="Software Engineer">Software Engineer</option>
-                    <option value="Team Manager">Team Manager</option>
-                    <option value="Sales Manager">Sales Manager</option>
-                    <option value="Area Manager">Area Manager</option>
+                    <option value="Software Engineer" <?php if($result['department'] == 'Software Engineer') 
+                    {
+                        echo "selected";
+                    }
+                    ?>>Software Engineer</option>
+                    <option value="Team Manager" <?php 
+                     if($result['department'] == 'Team Manager') 
+                    {
+                        echo "selected";
+                    }
+                    ?>>Team Manager</option>
+                    <option value="Sales Manager" <?php if($result['department']=='Sales Manager') 
+                    {
+                        echo"selected"
+                    ;}
+                    ?>>Sales Manager</option>
+                    <option value="Area Manager" <?php 
+                    if($result['department']=='Area Manager') 
+                    {
+                        echo"selected";
+                    }
+                    ?>>Area Manager</option>
                 </select>
-                
 
-                <textarea name="address" class="textfield ">Address</textarea>
-                <input type="Submit" value="Search" class="btn" style="background-color: gray;">
-                <input type="Submit"  name="save" value="save" class="btn" style="background-color: green;">
+
+                <textarea name="address"
+                    class="textfield "> <?php if(isset($_POST['searchdata'])){ echo $result['address']; }?> </textarea>
+                <input type="Submit" name="searchdata" value="Search" class="btn" style="background-color: gray;">
+                <input type="Submit" name="save" value="Save" class="btn" style="background-color: green;">
                 <input type="Submit" value="Modify" class="btn">
                 <input type="Submit" value="Delete" class="btn" style="background-color: red;">
                 <input type="Submit" value="Clear" class="btn" style="background-color: blue;">
 
             </div>
-      
+
+        </form>
+
     </div>
 
-    </form>
+
 
 </body>
 
